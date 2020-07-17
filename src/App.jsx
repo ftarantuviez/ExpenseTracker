14d90b3437ee8e79335cefc9e7edf0d3afcad617
francisco <ftarantuviez.dev@gmail.com>
2020-07-17 02:26:57 -0300

    Deleting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import {Header} from './components/Header'
 class App extends React.Component{
     render(){
         return(
             <h1>app</h1>
+            <Header />
         )
     }
 }
